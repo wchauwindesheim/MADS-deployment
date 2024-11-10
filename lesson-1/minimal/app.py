35d99b4cf5455fc5263adbd834da3194ec17195c
Raoul Grouls <Raoul.Grouls@han.nl>
2024-11-10 09:26:53 +0100

    minimal setup

---
+++ lesson-1/minimal/app.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'Hello from Python Container!')
+
+if __name__ == '__main__':
+    server = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
+    print('Starting server on port 8000')
+    server.serve_forever()
